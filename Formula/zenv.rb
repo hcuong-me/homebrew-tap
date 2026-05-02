1463fd8102b72510e996d738ddd842695a08059b
Cuong Hoang <hcuong.me@gmail.com>
2026-05-02 23:34:53 +0700

    Fix zenv version

--- Formula/zenv.rb
+++ Formula/zenv.rb
@@ -1,8 +1,9 @@
 class Zenv < Formula
   desc "Secure environment variable manager for zsh"
   homepage "https://github.com/hcuong-me/zenv"
-  url "https://github.com/hcuong-me/zenv/releases/download/v${VERSION}/zenv"
-  sha256 "${SHA256}"
+  version "0.0.1"
+  url "https://github.com/hcuong-me/zenv/releases/download/v0.0.1/zenv"
+  sha256 "12caa4bd12f5a6c49e53fe7b7e8f174d7580dbdec2606367b438056a99d62c6c"
   license "MIT"
 
   depends_on :macos
