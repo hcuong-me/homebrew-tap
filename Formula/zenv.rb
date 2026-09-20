939c25ac161a52fc6f7aff70c5351b0c34c5a0d3
hcuong-me <hcuong.me@gmail.com>
2026-09-20 22:25:11 +0700

    zenv: update to v1.0.0 (#4)

--- Formula/zenv.rb
+++ Formula/zenv.rb
@@ -1,9 +1,9 @@
 class Zenv < Formula
   desc "Secure environment variable manager for zsh"
   homepage "https://github.com/hcuong-me/zenv"
-  version "0.0.1"
-  url "https://github.com/hcuong-me/zenv/releases/download/v0.0.1/zenv"
-  sha256 "12caa4bd12f5a6c49e53fe7b7e8f174d7580dbdec2606367b438056a99d62c6c"
+  url "https://github.com/hcuong-me/zenv/releases/download/v1.0.0/zenv"
+  version "1.0.0"
+  sha256 "fbec938f826590204893d8c3dfbe7b14853714c9fd0e64794026b323cffc6c6d"
   license "MIT"
 
   depends_on :macos
